8ccb2723a83fd08f8264eda6b41b8149438111bb
Tim Hopkins <hopk0067@algonquinlive.com>
2019-03-29 14:48:22 -0400

    added ajax

--- js/dev/main.js
+++ js/dev/main.js
@@ -14836,4 +14836,33 @@ $('img').click(function () {
   console.log($(this).attr('src'))
   $(this).toggleClass('special')
 })
+
+/*AJAX*/
+$('#content').load('./about.html')
+$('#contentNav .nav-link').click(function (e) {
+  e.preventDefault()
+var page = $(this).attr('href')
+$('#contentNav .active').removeClass('active')
+$(this).addClass('active')
+$('#content').fadeOut(500, function () {
+$(this).load(page)
+}).fadeIn(500)
+}) // Closing click event on the contentNav nav-link
+
+$.ajax({
+  url: './data/posts.json',
+  type: 'GET',
+  dataType: 'json'
+}).done(function (data) {
+  var numPosts = data.posts.length
+  for(var i = 0; i < numPosts; i++) {
+    var post = '<div class="col-sm-6 p-5"><h3>'
+    post += (i+1) + '. ' + data.posts[i].title
+    post += '</h3><p>'
+    post += data.posts[i].body
+    post += '</p></div>'
+    $('#posts').append(post)
+  }
+
 })
+}) // Closing the document.ready method and the function
